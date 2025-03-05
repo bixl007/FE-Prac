07dead6cb72fcaff647067d100d9203df42b60f5
Bishal Baira <151175250+Bishalhere007@users.noreply.github.com>
2025-03-05 15:35:00 +0530

    Js loops, conditional

---
+++ 09_JsOperations/switch.js
@@ -0,0 +1,21 @@
+const score = 96;
+let grade;
+
+switch (true) {
+    case (score >= 90):
+        grade = "A";
+        break;
+    case (score >= 80):
+        grade = "B";
+        break;
+    case (score >= 70):
+        grade = "C";
+        break;
+    case (score >= 60):
+        grade = "D";
+        break;
+    default:
+        grade = "F";
+}
+
+console.log(grade);
