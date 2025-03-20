de634634502e50a252c0554c2adb3b4bb9ccf59f
Bishal Baira <bishalbaira432@gmail.com>
2025-03-20 12:47:32 +0530

    Array functions

--- 12_JsEssentials/array.js
+++ 12_JsEssentials/array.js
@@ -1,3 +1,25 @@
-let fruit = ['apple', 'banana', 'orange', 'grapes', 'mango'];
+let fruit = ["apple", "banana", "orange", "grapes", "mango"];
 console.log(fruit);
 console.log(fruit[0]);
+
+let number = new Array(1, 2, 3, 4, 5);
+console.log(number);
+
+console.log(number.length);
+console.log(number.indexOf(3));
+
+number.push(6);
+console.log(number);
+
+number.pop();
+console.log(number);
+
+number.shift();
+console.log(number);
+
+number.unshift(0);
+console.log(number);
+
+let vegetables = ["tomato", "potato", "onion"];
+vegetables.splice(1, 1, "brinjal");
+console.log(vegetables);
