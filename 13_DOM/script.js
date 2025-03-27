2ed59c853c1d5c6b24ad998fa93581e5ace010f6
Bishal Baira <bishalbaira432@gmail.com>
2025-03-27 12:33:28 +0530

    Add reset button and functionality to restore original text and styles

--- 13_DOM/script.js
+++ 13_DOM/script.js
@@ -9,3 +9,14 @@ const changeText = () => {
   items[3].style.backgroundColor = "purple";
   items[4].style.backgroundColor = "pink";
 };
+
+const reset = () => {
+    const p = document.querySelector("#message");
+    p.textContent = "Hello World, this is original text.";
+    let items = document.querySelectorAll(".items");
+    items[0].style.backgroundColor = "#333";
+    items[1].style.backgroundColor = "#333";
+    items[2].style.backgroundColor = "#333";
+    items[3].style.backgroundColor = "#333";
+    items[4].style.backgroundColor = "#333";
+}
